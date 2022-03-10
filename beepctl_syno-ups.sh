0ef33df9768005d5dd3ce639fd8c5efec7151627
Sergey T <98024075+tsvsys@users.noreply.github.com>
2022-03-10 18:28:23 +0200

    first commit

---
+++ beepctl_syno-ups.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+# original script https://moshib.in/2019/02/08/disable-ups-beeper-synology.html
+#
+# script modified to work with synology DNS v7
+# tested on DSM 7.0.1
+# 
+if [ "$1" == "curtime" ]
+then
+	echo "Using current time to set UPS beeper status"
+	H=$(date "+%k")
+	if (( "$H" >= 9 )) 
+	then
+		goal="enable"
+	else
+		goal="disable"
+	fi
+else
+	goal=$1
+fi
+
+if [ "$goal" == "enable" ] || [ "$goal" == "disable" ]
+then
+	echo "$goal beeper..."
+	python2 /root/upscmd.py beeper.${goal}
+	echo "Waiting 5 seconds for UPS to update state..."
+	sleep 5
+	if [[ "$(upsc ups ups.beeper.status)" == "${goal}d" ]]
+	then
+		echo "Beeper ${goal}d."
+	else
+		echo "Unable to $goal beeper. Status = $(upsc ups ups.beeper.status)."
+		exit 1
+	fi
+else
+	echo "Unknown / unsupported argument: $1"
+fi
+
